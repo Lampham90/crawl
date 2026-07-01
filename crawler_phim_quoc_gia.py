f6dc605c7a1b9c4b5853ba23d8a1d1398d530fc3
Lampham90 <111651253+Lampham90@users.noreply.github.com>
2026-07-01 15:26:30 +0700

    Update crawler_phim_quoc_gia.py

--- crawler_phim_quoc_gia.py
+++ crawler_phim_quoc_gia.py
@@ -4,7 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 BASE_URL = "https://phimapi.com/v1/api"
 YEARS = [2026, 2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017]
 LIMIT_COUNT = 300
-MAX_WORKERS = 2 # Tăng lên cho nhanh ní ơi
+MAX_WORKERS = 1 # Tăng lên cho nhanh ní ơi
 OUTPUT_DIR = "data_categories"
 
 if not os.path.exists(OUTPUT_DIR): os.makedirs(OUTPUT_DIR)
